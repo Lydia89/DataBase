c7424d7fca078f3835d9e368f69e4d00b8101380
Lydia89 <lydy@live.fr>
2020-11-06 22:49:49 +0100

    SQL

--- Students.sql
+++ Students.sql
@@ -103,3 +103,56 @@ SELECT name FROM `Students` WHERE (city='Lyon')/* Steeven Nour*/
 
 /*Rapport lvl 2*/
 
+SELECT * FROM Students LEFT JOIN Favorites ON Students.id = Favorites.Students_id where Students.id = 5
+SELECT name,sport FROM Students LEFT JOIN Favorites ON Students.id = Favorites.Students_id where Students.id = 4 /*Nour
+Baseball*/
+
+SELECT name,class FROM Students LEFT JOIN Favorites ON Students.id = Favorites.Students_id where Students.id = 1/*Véronique Arts*/
+SELECT * FROM Students LEFT JOIN Favorites ON Students.id = Favorites.Students_id where class = 'music'/*6
+Sophie
+Paris
+2
+Music
+Hip-hop
+6*/
+SELECT name FROM Students LEFT JOIN Favorites ON Students.id = Favorites.Students_id where sport = 'Tennis'/*Marc Romain*/
+SELECT name FROM Students LEFT JOIN Favorites ON Students.id = Favorites.Students_id where class = 'Arts'/*Véronique
+Nour*/
+SELECT COUNT(*) FROM Students WHERE city= 'paris'/* 3*/
+
+/*Rapport lvl 3*/
+
+SELECT * FROM Students LEFT JOIN Studentslanguages ON Students.id = Studentslanguages.Students_id LEFT JOIN Languages ON Studentslanguages.Languages_id = Languages.id WHERE Students.id = 1
+
+  /*1 Véronique Paris1 111French
+ 
+1 Véronique Paris2 1 2 2 English*/
+
+SELECT * FROM Students LEFT JOIN Studentslanguages ON Students.id = Studentslanguages.Students_id LEFT JOIN Languages ON Studentslanguages.Languages_id = Languages.id WHERE Students.id = 4
+/*4 Nour Lyon 6 4 1 1French
+4 Nour Lyon 7 4 2 2 English
+4 Nour Lyon 8 4 4 4 Spanish 
+4 Nour Lyon 9 4 5 5 Mandarin
+*/
+SELECT Students.name ,Languages.name FROM Students LEFT JOIN Studentslanguages ON Students.id = Studentslanguages.Students_id LEFT JOIN Languages ON Studentslanguages.Languages_id = Languages.id WHERE Students.id = 5
+/*Romain French
+Romain Mandarin*/
+
+
+SELECT Students.name COUNT(Languages.name) FROM Languages LEFT JOIN Studentslanguages ON Languages.id = Studentslanguages.Languages_id LEFT JOIN Students ON Studentslanguages.Students_id = Students.id WHERE Students.id = 6
+/*Sophie 3*/
+
+
+/*Bonus*/
+SELECT name FROM `Students` WHERE name LIKE '%e%'
+/*  Véronique Steeven Sophie  */
+SELECT sport FROM Students LEFT JOIN Favorites ON Students.id = Favorites.Students_id where name LIKE '%e%'
+/*Boxing Cricket Hip-hop*/
+
+
+SELECT class FROM Students LEFT JOIN Favorites ON Students.id = Favorites.Students_id where city LIKE '%i%'
+/*class :
+Arts
+Literature
+Computer science
+Music*/
